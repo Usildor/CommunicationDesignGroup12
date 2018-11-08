190f399b8825f041fd08d1ebd9589a7c563e1123
SabineGoe <sabine-corinna.goehringer@stud.uni-bamberg.de>
2018-11-08 11:05:19 +0100

    Slideshow with the youth club pictures
    (not quite yet working...)

--- document.js
+++ document.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(window).scroll( function(){
-		
+
 		var top_of_window = $(window).scrollTop() /*+ $(window).height();*/
 		//fade-in
 		$('.fadeSwitch').each(function(){
@@ -37,7 +37,7 @@ anime.timeline({loop: true})
   });
 /*$(function(){
 	$(window).scroll( function(){
-		
+
 		var bottom_of_window = $(window).scrollTop() + $(window).height();
 		//fade-in
 		$('.fade-ani').each(function(){
@@ -52,3 +52,32 @@ anime.timeline({loop: true})
 
 	});
 });*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
